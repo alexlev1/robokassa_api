e6c670210e6c4229a3d5e55ef2bc96fdc4ad635f
Alex Levashov <alevash1@gmail.com>
2019-01-09 22:26:00 +0300

    Refactor payment url

--- lib/robokassa_api/payment_url.rb
+++ lib/robokassa_api/payment_url.rb
@@ -7,22 +7,20 @@ module RobokassaApi
     def self.create_pay_url(signature, user, out_sum, description)
       url = pay_link(signature, user, out_sum, description)
 
-      if test_mode?
-        url + "&IsTest=1"
-      else
-        url
-      end
+      url += "&IsTest=1" if test_mode?
     end
+
+    def test_mode?
+      Robokassa.mode == :test
+    end
+
+    private
 
     def pay_link(signature, user, out_sum, description)
       URL + "MerchantLogin=#{MERCHANT_LOGIN}" +
         "&OutSum=#{out_sum}" + "&InvId=#{INVOICE_ID}" +
         "&Desc=#{description}" + "&SignatureValue=#{signature}" +
         "&Culture=#{CULTURE}" + "&Encoding=#{ENCODING}" + "&Shp_user=#{user.id}"
-    end
-
-    def test_mode?
-      Robokassa.mode == :test
     end
   end
 end
